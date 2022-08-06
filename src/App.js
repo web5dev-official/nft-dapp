3e29353b5ddabdfb1761cd2c8c69e279cbc84be5
ZEROMOON <88079157+zeromoon-defi@users.noreply.github.com>
2022-08-06 18:18:13 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
+
   const [NftStatus, setNftStatus] = useState('none');
   const [ClaimNft, setClaimNft] = useState('visible');
   const [BtnStatus, setBtnStatus] = useState("Connect to Mint");
   const [Wallet, setWallet] = useState();
   const [MintAmount, setMintAmount] = useState();
-  const [Status, setStatus] = useState();
+  const [Status, setStatus] = useState('something wrong in alert component');
+  const [Alert, setAlert] = useState()
+
+  const ShowAlert = () => {
+    
+  }
 
   const ClickHandler = async () => {
     if (BtnStatus === "Connect to Mint") {
@@ -50,6 +56,7 @@ function App() {
         alt='cover'
           className=" w-full min-h-screen "
         />
+        <div className='absolute border-l-2 border-blue-400/60 top-2 right-0 bg-pink-700/50 text-lg text-white bold pl-6 pr-1'>{Status}</div>
        <div className='absolute h-full w-full'>
         <div className='flex flex-col h-screen w-screen justify-center items-center'>
           <div className='w-auto h-auto'>
@@ -64,8 +71,8 @@ function App() {
        <input  className='bg-transparent border-b-2 s w-8 mx-2'></input>
        <h1 className='text-white text-[1.4rem] bold  inline'>{`LiquiDoodles`}</h1>
        </div>
-       <h1 className='text-white text-[0.8rem]  mt-4 bold'>{`your price will be 0 ETH and you will get 20 nft`}</h1>
-       <h1 className='text-white text-[0.8rem] my-2 bold'>{`The LiquiDoodles you get, will be randomly picked`}</h1>
+       <h1 className='text-white text-[1rem]  mt-4 bold'>{`your price will be 0 ETH and you will get 20 nft`}</h1>
+       <h1 className='text-white text-[1rem] my-2 bold'>{`The LiquiDoodles you get, will be randomly picked`}</h1>
        <h1 className='text-white text-[2.2rem] my-2 bold'>{`BEST OF LUCK`}</h1>
        <button className='bg-white my-2 text-blue-300 px-4 py-2 rounded-[16px]'
        onClick={ClickHandler}>{BtnStatus}</button>
