8d411a86dd0b21c156594a5e0815fe9360847f44
FIRESTAR-OFFICIAL <88079157+zeromoon-defi@users.noreply.github.com>
2022-08-07 23:12:16 +0530

    updated

--- src/utils/interact.js
+++
@@ -1,5 +0,0 @@
-const { createAlchemyWeb3 } = require('@alch/alchemy-web3');
-const web3 = createAlchemyWeb3("XRznHNhMx9axnrPTil7nMXgJf5uDJ5A9");
-const Abi = require("../contract/abi.json");
-const address = "0xc8860359d57F0965D88F5aeEB82Dfc6106822Fc0";
-const NFtContract = new web3.eth.Contract(address,Abi);
